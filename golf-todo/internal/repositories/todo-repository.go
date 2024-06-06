eba98209dcddd018c98da363d881cf04cbc6c617
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-06-06 13:31:49 +0200

    update

--- golf-todo/internal/repositories/todo-repository.go
+++ golf-todo/internal/repositories/todo-repository.go
@@ -1,3 +1,19 @@
 package repositories
 
-type TodoRepository struct{}
+import (
+	"database/sql"
+	"todo/internal/services"
+)
+
+type TodoRepository struct {
+	db *sql.DB
+}
+
+func NewTodoRepository(db *sql.DB) *TodoRepository {
+	return &TodoRepository{db: db}
+}
+
+func (r *TodoRepository) Insert(todo services.Todo) error {
+	_, err := r.db.Exec("INSERT INTO todoschema.todos (id, title) VALUES ($1, $2)", todo.Title)
+	return err
+}
--- golf-todo/internal/repositories/todo-repository_test.go
+++ golf-todo/internal/repositories/todo-repository_test.go
@@ -1,7 +1,43 @@
 package repositories
 
-import "testing"
+import (
+	"testing"
+	"todo/internal/services"
 
-func TestGetTodos(t *testing.T) {
-	// TODO: Implement test
+	"github.com/mariusfa/golf/database"
+)
+
+func TestInsertAndGetTodos(t *testing.T) {
+	db, err := database.Setup(&dbConfig)
+	if err != nil {
+		t.Errorf("Error setting up database: %v", err)
+	}
+	defer db.Close()
+
+	TodoRepository := NewTodoRepository(db)
+
+	todoToInsert := services.Todo{
+		Title: "Test todo",
+	}
+
+	if err := TodoRepository.Insert(todoToInsert); err != nil {
+		t.Errorf("Error inserting todo: %v", err)
+	}
+
+	todos, err := TodoRepository.GetTodos()
+	if err != nil {
+		t.Errorf("Error getting todos: %v", err)
+	}
+
+	if len(todos) != 1 {
+		t.Errorf("Expected 1 todo, got %v", len(todos))
+	}
+
+	if todos[0].Title != todoToInsert.Title {
+		t.Errorf("Expected todo title to be %v, got %v", todoToInsert.Title, todos[0].Title)
+	}
+
+	if todos[0].Id == "" {
+		t.Errorf("Expected todo ID to be set, got %v", todos[0].Id)
+	}
 }
